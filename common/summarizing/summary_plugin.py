f9542f6f231acc4d0e0719f209a52aa0f17989de
Xenon <lgasyou@163.com>
2020-11-23 15:21:46 +0800

    chore(common): 使用submodule解决common模块的同步问题

--- common/summarizing/summary_plugin.py
+++
@@ -1,4 +0,0 @@
-class SummaryPlugin(object):
-
-    def write_summary(self, pods, now: str, summary_name: str):
-        pass
--- common/utils/kube_config.py
+++
@@ -1,7 +0,0 @@
-import kubernetes
-
-from .. import consts
-
-
-def load_kube_config(filename: str = consts.KUBE_CONFIG_FILENAME):
-    kubernetes.config.kube_config.load_kube_config(config_file=filename)
