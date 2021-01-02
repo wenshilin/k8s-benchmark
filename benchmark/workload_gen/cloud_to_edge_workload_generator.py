8b6c7e1e766ffb7c3f79f6dd8ec5b58748acf329
github_name <2439774702@qq.com>
2021-01-02 18:51:32 +0800

    test simulation

--- benchmark/workload_gen/cloud_to_edge_workload_generator.py
+++ benchmark/workload_gen/cloud_to_edge_workload_generator.py
@@ -15,7 +15,7 @@ class Cloud2EdgeWorkloadGenerator(WorkloadGenerator):
     def __init__(self):
         super().__init__(consts.TASK_TYPES[:2])
         #self.poisson_dist = stats.poisson.rvs(mu=75000, size=1000, random_state=1)  #bra
-        # self.poisson_dist = stats.poisson.rvs(mu=135000, size=1000, random_state=1)  # mrp
+        # self.poisson_dist = stats.poisson.rvs(mu=140000, size=1000, random_state=1)  # mrp
         self.poisson_dist = stats.poisson.rvs(mu=150000, size=1000, random_state=1)  #lrp
 
     def _generate_job(self):
